1aced333e0ed69f2b36671907d1a4105b0394396
Alexandre Bezerra Barbosa <alexandre_b_barbosa@yahoo.com.br>
2018-06-28 11:16:08 -0300

    Update Conexao.php

--- Conexao.php
+++ Conexao.php
@@ -10,7 +10,7 @@ class Conexao
     public static function getInstance()
     {
         if (is_null(self::$conexao)) {
-            self::$conexao = new \PDO('mysql:host=localhost;port=3306;dbname=projeto_mvc', 'root', 'P@ssw0rd');
+            self::$conexao = new \PDO('mysql:host=localhost;port=3306;dbname=projeto_mvc', 'root', '123456');
             self::$conexao->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
             self::$conexao->exec('set names utf8');
         }
